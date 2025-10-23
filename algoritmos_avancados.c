1683ab3e1922da434723b812d6345d7dc2ef52aa
Gabriel B Lisboa <gabrielblisboa1800@gmail.com>
2025-10-23 22:35:30 +0000

    Funçao de geraçao de salas

--- algoritmos_avancados.c
+++ algoritmos_avancados.c
@@ -8,3 +8,32 @@ typedef struct Sala {
     struct Sala *direita;
 } Sala;
 
+// Função que cria uma nova sala
+Sala* criarSala(const char *nome) {
+    Sala* nova = (Sala*) malloc(sizeof(Sala));
+    strcpy(nova->nome, nome);
+    nova->esquerda = NULL;
+    nova->direita = NULL;
+    return nova;
+}
+
+// Função que cria toda a árvore da mansão
+Sala* criarMansao() {
+    // Nó raiz
+    Sala* hall = criarSala("Hall de Entrada");
+
+    // Primeira ramificação
+    hall->esquerda = criarSala("Sala de Estar");
+    hall->direita  = criarSala("Cozinha");
+
+    // Subárvore esquerda
+    hall->esquerda->esquerda = criarSala("Biblioteca");
+    hall->esquerda->direita  = criarSala("Jardim");
+
+    // Subárvore direita
+    hall->direita->esquerda = criarSala("Despensa");
+    hall->direita->direita  = criarSala("Garagem");
+
+    return hall;
+}
+
